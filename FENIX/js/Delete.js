a1089b794fb6a4697041a3c819b914665143381f
Gonzalo Chiaravalle <97534308+gonzachiara@users.noreply.github.com>
2022-07-10 19:38:09 -0300

    Add files via upload

---
+++ FENIX/js/Delete.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    busco = document.getElementById("last").remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+}, 3000)
